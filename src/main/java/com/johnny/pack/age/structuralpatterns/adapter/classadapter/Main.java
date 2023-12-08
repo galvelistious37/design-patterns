ceb3832763f2a1883f455989c16d0b8121ef038c
johnny <john.robertson88@yahoo.com>
2023-12-08 00:37:13 -0800

    Add adapter pattern examples

---
+++ src/main/java/com/johnny/pack/age/structuralpatterns/adapter/classadapter/Main.java
@@ -0,0 +1,18 @@
+package com.johnny.pack.age.structuralpatterns.adapter.classadapter;
+
+public class Main {
+
+    public static void main(String[] args) {
+        EmployeeClassAdapter adapter = new EmployeeClassAdapter();
+        populateEmployeeData(adapter);
+        BusinessCardDesigner designer = new BusinessCardDesigner();
+        String card = designer.designCard(adapter);
+        System.out.println(card);
+    }
+
+    public static void populateEmployeeData(Employee employee) {
+        employee.setFullName("Elliot Alderson");
+        employee.setJobTitle("Security Engineer");
+        employee.setOfficeLocation("Allsafe Cybersecurity, New York City, New York");
+    }
+}
