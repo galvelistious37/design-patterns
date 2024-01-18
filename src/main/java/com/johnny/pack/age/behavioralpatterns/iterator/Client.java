b57e1e248ab2689aee83212fb7834a3eab5bc1f8
johnny <john.robertson88@yahoo.com>
2024-01-17 21:52:50 -0800

    Add iterator design pattern example

---
+++ src/main/java/com/johnny/pack/age/behavioralpatterns/iterator/Client.java
@@ -0,0 +1,10 @@
+package com.johnny.pack.age.behavioralpatterns.iterator;
+
+public class Client {
+    public static void main(String[] args) {
+        Iterator<ThemeColor> iter = ThemeColor.getIterator();
+        while(iter.hasNext()){
+            System.out.println(iter.next());
+        }
+    }
+}
