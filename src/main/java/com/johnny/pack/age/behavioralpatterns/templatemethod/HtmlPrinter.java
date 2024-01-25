b31dbd48426c04c3f6405087cf156c398fc72e2c
johnny <john.robertson88@yahoo.com>
2024-01-24 22:13:56 -0800

    Add example for template method design pattern

---
+++ src/main/java/com/johnny/pack/age/behavioralpatterns/templatemethod/HtmlPrinter.java
@@ -0,0 +1,40 @@
+package com.johnny.pack.age.behavioralpatterns.templatemethod;
+
+import java.util.Map;
+
+// Concrete implementation. Implements steps needed
+// by template method
+public class HtmlPrinter extends OrderPrinter{
+    @Override
+    protected String start() {
+        return "<html><head><title>Order Details</title></head><body>";
+    }
+
+    @Override
+    protected String formatOrderNumber(Order order) {
+        return "<h1>Order # " + order.getId() + "</h1>";
+    }
+
+    @Override
+    protected String formatItems(Order order) {
+        StringBuilder builder = new StringBuilder("<p><ul>");
+        for(Map.Entry<String, Double> entry : order.getItems().entrySet()){
+            builder.append("<li>" + entry.getKey())
+                    .append(": $")
+                    .append(entry.getValue())
+                    .append("</li>");
+        }
+        builder.append("</ul></p>");
+        return builder.toString();
+    }
+
+    @Override
+    protected String formatTotal(Order order) {
+        return "<br/><hr/><h3>Total: $" + order.getTotal() + "</h3>";
+    }
+
+    @Override
+    protected String end() {
+        return "</body></html>";
+    }
+}
---
+++ src/main/java/com/johnny/pack/age/behavioralpatterns/templatemethod/TextPrinter.java
@@ -0,0 +1,40 @@
+package com.johnny.pack.age.behavioralpatterns.templatemethod;
+
+import java.util.Map;
+
+// Concrete implementation. Implements steps needed
+// by template method
+public class TextPrinter extends OrderPrinter{
+    @Override
+    protected String start() {
+        return "Order Details";
+    }
+
+    @Override
+    protected String formatOrderNumber(Order order) {
+        return "order #: " + order.getId();
+    }
+
+    @Override
+    protected String formatItems(Order order) {
+        StringBuilder builder = new StringBuilder("Items\n---------------\n");
+        for(Map.Entry<String, Double> entry : order.getItems().entrySet()){
+            builder.append(entry.getKey())
+                    .append(": $")
+                    .append(entry.getValue())
+                    .append("\n");
+        }
+        builder.append("------------------");
+        return builder.toString();
+    }
+
+    @Override
+    protected String formatTotal(Order order) {
+        return "Total: $" + order.getTotal();
+    }
+
+    @Override
+    protected String end() {
+        return "Footer and stuff";
+    }
+}
